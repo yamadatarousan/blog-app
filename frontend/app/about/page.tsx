bd3b7a4545048003b30040929c50291510ae8bfb
sohei.kakihara <souhei_kakihara@simple-inc.com>
2025-04-24 20:10:18 +0900

    Aboutページ追加

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default async function About() {
+    return (
+    <div className="container mx-auto p-6">
+        <h1 className="text-4xl font-bold text-primary mb-8 text-center animate-fade-in">
+        About
+        </h1>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        自分用のブログアプリを作っている最中です
+        </div>
+    </div>
+    );
+}
