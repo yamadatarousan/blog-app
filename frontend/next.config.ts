be1f08b8ce3b5c0fb179918b00a9a59d59feb18f
sohei.kakihara <souhei_kakihara@simple-inc.com>
2025-04-29 18:45:29 +0900

    railwayビルド用設定変更

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true, // 型エラーを無視
-  },  
-  eslint: {
-    ignoreDuringBuilds: true, // ビルド時にESLintエラーを無視
-  },
+module.exports = {
+  output: 'export',
+  distDir: 'out',
 };
-
-module.exports = nextConfig;
