e4afc445d2b1452e61fa8de072c3360e4c9e2d28
sohei.kakihara <souhei_kakihara@simple-inc.com>
2025-04-29 19:08:51 +0900

    ビルド用に設定変更

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,13 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',
   distDir: 'out',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'your-image-domain.com', // 例: s3.amazonaws.com
+      },
+    ],
+  },
 };
