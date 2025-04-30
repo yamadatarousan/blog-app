652af78bbed466ab7a8ab1bd80c63483c141b347
sohei.kakihara <souhei_kakihara@simple-inc.com>
2025-04-30 09:22:49 +0900

    管理画面の編集項目が表示されない不具合を修正

--- backend/app/Providers/Filament/AdminPanelProvider.php
+++ backend/app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,8 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Filament\Resources\PostResource;
+use App\Filament\Resources\CommentResource;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -53,6 +55,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class, // カスタムミドルウェアを削除し標準に戻す
+            ])
+            ->resources([
+                CommentResource::class,
+                PostResource::class,
             ]);
     }
 }
